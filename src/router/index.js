3513aa44a5ea206e5c8f70b755654001130d35ae
luojianaaa <2503282699@qq.com>
2021-07-19 23:54:49 +0800

    收工

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// 导入路由模块
+import centerRouter from '@/router/routes/center'
+import cinemaRouter from '@/router/routes/cinema'
+import filmRouter from '@/router/routes/film'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // 注册路由模块
+  centerRouter,
+  cinemaRouter,
+  filmRouter,
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    redirect: '/film'
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
